cc8f28d0f2135f4f86caa804972fd6fee2a8f066
iC4rds <dstaud2005@gmail.com>
2025-09-21 17:04:33 +0200

    feat: add bun

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,11 @@ fastfetch --config arch
 
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /home/david/.lmstudio/bin
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
